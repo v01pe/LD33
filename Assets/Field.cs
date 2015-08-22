2ad303eea95c9ef7320a74d6c5d4c4663be43e5e
Jan Hackl <jan@brokenrul.es>
2015-08-22 22:10:40 +0200

    Started working on a field class that genertes an texture on the fly

---
+++ Assets/Field.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : MonoBehaviour
+{
+	public int height = 480;
+
+	private Color[] pixels;
+	private int width;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Camera camera = Camera.main;
+		float camHeight = camera.orthographicSize * 2f;
+//		float camWidth = height * camera.aspect;
+
+		width = (int)(height * camera.aspect);
+		float texelRatio = (float)height / camHeight;
+
+		Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = Sprite.Create(texture, new Rect(0,0,width,height), new Vector2(0.5f,0.5f), texelRatio);
+
+		pixels = texture.GetPixels();
+		for (int i = 0; i < pixels.Length; i++)
+		{
+			pixels[i] = Color.green;
+			pixels[i].a = 255;
+		}
+		texture.SetPixels(pixels);
+		texture.Apply();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
