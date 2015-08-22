79c7692c26f2acf106883e153223ab6ba8cdf0b9
Jan Hackl <jan@brokenrul.es>
2015-08-22 22:54:09 +0200

    Created Monster script that paints itself into the field

--- Assets/Field.cs
+++ Assets/Field.cs
@@ -5,6 +5,7 @@ public class Field : MonoBehaviour
 {
 	public int height = 480;
 
+	Texture2D texture;
 	private Color[] pixels;
 	private int width;
 
@@ -18,7 +19,7 @@ public class Field : MonoBehaviour
 		width = (int)(height * camera.aspect);
 		float texelRatio = (float)height / camHeight;
 
-		Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
 		
 		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
 		spriteRenderer.sprite = Sprite.Create(texture, new Rect(0,0,width,height), new Vector2(0.5f,0.5f), texelRatio);
@@ -26,16 +27,42 @@ public class Field : MonoBehaviour
 		pixels = texture.GetPixels();
 		for (int i = 0; i < pixels.Length; i++)
 		{
-			pixels[i] = Color.green;
-			pixels[i].a = 255;
+			pixels[i] = Color.white;
 		}
 		texture.SetPixels(pixels);
-		texture.Apply();
+		texture.Apply(false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		texture.SetPixels(pixels);
+		texture.Apply(false);
+	}
+	
+	public void PaintDot(Color color, Vector2 position, float size)
+	{
+		float sqrRadius = size * size / 4;
+		Vector2 extents = Vector2.one * size;
+		
+		Rect rect = new Rect(position - extents/2, extents);
+		if (rect.xMin < 0) rect.xMin = 0;
+		if (rect.yMin < 0) rect.yMin = 0;
+		if (rect.xMax > width-1) rect.xMax = width-1;
+		if (rect.yMax > height-1) rect.yMax = height-1;
 
+		for (int x=(int)rect.xMin; x<=rect.xMax; x++)
+		{
+			for (int y=(int)rect.yMin; y<=rect.yMax; y++)
+			{
+				float dX = position.x - x;
+				float dY = position.y - y;
+				if (dX*dX + dY*dY <= sqrRadius)
+				{
+					int i = x + y*width;
+					pixels[i] = color;
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Monster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour
+{
+	public Field field;
+	public float size = 10f;
+
+	private Vector2 position;
+	// Use this for initialization
+	void Start ()
+	{
+		if (field == null)
+		{
+			enabled = false;
+		}
+		else
+		{
+			position = Vector2.one * 300;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		field.PaintDot(Color.green, position, size);
+	}
+}
